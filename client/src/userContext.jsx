18c1a578f76f708917389ac504e5d6ff516e1a79
Raghav <125369338+raghav13d@users.noreply.github.com>
2024-07-04 21:02:24 +0530

    Update userContext.jsx

--- client/src/userContext.jsx
+++ client/src/userContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const UserContext=createContext({});
 export function UserContextProvider({children}) {
     const [userInfo,setUserInfo]=useState({});
-    const url='https://blogify-backend-2i1x.onrender.com'
+    const url='https://blogify-backend-hd17.onrender.com'
 
     return(
         <UserContext.Provider value={{userInfo,setUserInfo,url}}>
