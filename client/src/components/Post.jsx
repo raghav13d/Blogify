695d2d8d25a0a583c14ee438a06fa53ed3aa0974
Raghav <125369338+raghav13d@users.noreply.github.com>
2024-07-04 22:19:54 +0530

    Update Post.jsx

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -8,7 +8,7 @@ const  Post=({_id,title,summary,content,cover,createdAt,author})=>{
         <div className='post'>
         <div className='image'>
           <Link to={`/post/${_id}`}>
-            <img src={'http://localhost:4000/'+cover} alt=''></img>
+            <img src={url+'/'+cover} alt=''></img>
           </Link>
         
         </div>
